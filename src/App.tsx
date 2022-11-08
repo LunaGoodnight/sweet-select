7f54325f7b89f349dc306ce531a632ae71abbad0
Luna Goodnight <luna.lee@nextfortune.tw>
2022-11-08 13:35:50 +0800

    [Add] demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
+import React from "react";
+import { TreeSelector } from "./components/treeSelect/TreeSelector";
+import { useSweetTree } from "./components/treeSelect/useSweetTree";
+import { FilterBlock, Title } from "./styles/demoStyle";
+import GlobalStyle from "./styles/globalStyle";
+import { treeData } from "./data/treeData";
+import "./App.css";
 
-import './App.css';
-import GlobalStyle from './styles/globalStyle';
-import {TreeSelector} from "./components/treeSelect/TreeSelector";
-import {useSweetTree} from "./components/treeSelect/useSweetTree";
-import {treeData} from "./data/treeData";
-import styled from "styled-components";
-export const FilterBlock = styled.div`
-
-  width: 13rem;
-`
 function App() {
-  const treeSelect = useSweetTree({ list: treeData })
+  const treeSelect = useSweetTree({ list: treeData });
   return (
     <div className="App">
       <GlobalStyle />
       <header className="App-header">
         <FilterBlock>
-
+          <Title>Tree</Title>
           <TreeSelector {...treeSelect} />
         </FilterBlock>
       </header>
---
+++ src/styles/demoStyle.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Title = styled.div`
+  padding-bottom: 0.6rem;
+  font-size: 1.3rem;
+  text-align: left;
+  width: 100%;
+`;
+export const FilterBlock = styled.div`
+  width: 13rem;
+  display: flex;
+  flex-direction: column;
+`;
